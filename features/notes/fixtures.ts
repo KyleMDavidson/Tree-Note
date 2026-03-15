aa22311129491f9aa86b43af8762e5739b221e04
KyleMDavidson <ksonofdavid@gmail.com>
2026-03-15 12:28:46 -0400

    nomenclature (no need for screens); fix crash from undefined gesture responder; wq :q kupdate git ignore

---
+++ features/notes/fixtures.ts
@@ -0,0 +1,92 @@
+import { Node } from "./types";
+
+export const TestRoot: Node = {
+        id: 1,
+        children: [
+            {
+                id: 2,
+                children: [
+                    {
+                        id: 3,
+                        children: [
+                            {id: 7, children: [], title: "node 7 depth 4", content: "this is a leaf"}
+                        ],
+                        title: 'node 3 depth: 3',
+                        content: 'this is a leaf',
+                    }
+                ],
+                title: 'node 2 depth: 2',
+                content: 'this is an internal node',
+            },
+    {
+                id: 4,
+                children: [
+                    {
+                        id: 5,
+                        children: [{
+                            id: 6,
+                            children: [{id: 7, children: [], title: "node 7 depth 4", content: "this is a leaf"}],
+                            title: "node 6 depth 3",
+                            content: "this is an internal node"
+
+                        }],
+                        title: 'node 5 depth: 3',
+                        content: 'this is an internal node',
+                    }
+                ],
+                title: 'node 4 depth: 2',
+                content: 'this is an internal node',
+            },
+
+
+        ],
+        title: 'node 1 depth: 1',
+        content: 'this is a root node',
+}
+
+
+export const ContentfulTestRoot: Node = {
+  id: 1,
+  children: [
+    {
+      id: 2,
+      children: [
+        {
+          id: 3,
+          children: [{ id: 7, children: [], title: "groceries", content: "milk, eggs, flour" }],
+          title: "kitchen",
+          content: "",
+        },
+        {
+          id: 9,
+          children: [{id: 13, children: [], title: "flowers", content: "call james about the tree over fence"}],
+          title: "yard",
+          content: "",
+        },
+      ],
+      title: "home",
+      content: "",
+    },
+    {
+      id: 4,
+      children: [
+        {id:9, title: "prepare for presentation Friday", content:"papers: Manilov et al 1984, Pushkin 2001, Zenov et al 2024", children: []}
+      ],
+      title: "work",
+      content: "",
+    },
+    {
+      id: 8,
+      children: [
+        {id:10, title: "rehearsals", content: "Monday 6-9, Saturday 1-6", children: []},
+        {id: 11, title: "shows", content: "", children: [
+            {id: 12, title: "Spirit", content: "mic check @ 5. They have an M90 amp but we need to bring our own pedals.", children: []}
+        ]}
+      ],
+      title: "band",
+      content: "",
+    },
+  ],
+  title: "root",
+  content: "this is a root node",
+};
---
+++ features/notes/types.ts
@@ -0,0 +1,38 @@
+type Leaf = Node
+
+type Node = {
+    title: string;
+    content: string;
+    id: number;
+    children: Node[]
+}
+
+type MarkedNode = Node & {
+  isOnPathToFocused?: boolean;
+};
+
+
+type TouchState = {
+    startX: number;
+    startY: number;
+    currentX: number;
+    currentY: number;
+    isTracking: boolean;
+}
+
+type NodeTouchableBounds = {
+    [id: number]:{
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    }
+}
+
+type HoverState = {
+    isHovering: boolean;
+    hoverStartTime: number | null;
+    hoveredNodeId: number | null;
+}
+
+export type { HoverState, Leaf, MarkedNode, Node, NodeTouchableBounds, TouchState };
